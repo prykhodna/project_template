34838db2bd22d4501ab18cf94ebd8a7ee8832abb
Kseniia Prykhodna <kseniaprikhodnaia@gmail.com>
2025-04-06 18:22:41 +0300

    Created empty functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def enter_text_from_console():
+    pass
+
+
+def reading_files_with_python():
+    pass
+
+
+def reading_files_with_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def print_text_to_console():
+    pass
+
+
+def write_output_to_file_with_python():
+    pass
+
+
+def write_output_to_file_with_pandas():
+    pass
